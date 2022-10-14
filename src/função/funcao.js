8a78a7f546d74f4f5336a726aae265907ffe5535
Ramon Machado <ramonmachado383@yahoo.com.br>
2022-10-14 03:40:34 -0300

    Initial commit

---
+++ src/função/funcao.js
@@ -0,0 +1,12 @@
+function typeWriter(elemento) {
+    const textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = '';
+    textoArray.forEach((letra, i) => {
+        setTimeout(() =>
+            elemento.innerHTML += letra, 300 * i)
+    });
+
+}
+
+const titulo = document.querySelector('h1')
+typeWriter(titulo); s
